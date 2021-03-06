a04bd4b2ea848135533a10968a56ef33437a7543
Anastsiia <78366371+Anastsiia@users.noreply.github.com>
2021-03-06 15:11:00 +0300

    Create rush04.c

---
+++ rush04.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctifany <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/24 16:48:18 by ctifany           #+#    #+#             */
+/*   Updated: 2021/01/24 18:51:34 by ctifany          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_char(int x, char f_symb, char m_symb, char l_symb)
+{
+	ft_putchar(f_symb);
+	while (x - 1 > 1)
+	{
+		ft_putchar(m_symb);
+		x--;
+	}
+	if (x > 1)
+	{
+		ft_putchar(l_symb);
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	if (x > 0 && y > 0)
+	{
+		ft_print_char(x, 'A', 'B', 'C');
+		y--;
+		while (y > 1)
+		{
+			ft_print_char(x, 'B', ' ', 'B');
+			y--;
+		}
+		if (y > 0)
+		{
+			ft_print_char(x, 'C', 'B', 'A');
+		}
+	}
+}
